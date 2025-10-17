cee78a1f416128e941a85469c768ee22e37aed6f
MeltedButter77 <meltedbutterdev@gmail.com>
2025-10-17 19:43:24 +1100

    Added Hole Collisions

--- main.py
+++ main.py
@@ -8,15 +8,6 @@ async def main():
     screen = pygame.display.set_mode((800, 600))
     clock = pygame.time.Clock()
 
-    # Wait for user to tap/click to start (required on mobile)
-    print("Waiting for user interaction...")
-    await asyncio.sleep(0)  # allow pygbag to initialize
-
-    # Wait for a click/tap before starting
-    while not pygame.event.peek((pygame.MOUSEBUTTONDOWN, pygame.FINGERDOWN)):
-        await asyncio.sleep(0.05)
-    print("Starting game loop!")
-
     table = Table(screen, "8-ball")
 
     while True:
--- objects.py
+++ objects.py
@@ -19,6 +19,8 @@ class Ball:
         self.position = pygame.math.Vector2(position)
         self.velocity = pygame.math.Vector2((0, 0))
         self.radius = ball_radius
+        self.id = id
+        self.on_board = True
 
         self.drag = 0.02  # Higher = More drag
         self.cue_strength = 0.1  # Higher = Stronger
@@ -58,8 +60,16 @@ class Table:
         self.mouse_down_pos = None
 
         self.balls = []
+        self.holes = []
 
-        self.wall_offset = 15
+        self.wall_offset = 20  # wall thickness
+        self.hole_offset = self.wall_offset * 1  # 1 = hole center on wall, <1 = hole inside wall, >1 = hole more on felt
+        self.hole_radius = ball_radius * 1.5
+        self.hole_strength = 0.7  # Higher = stronger
+
+        for x in range(3):
+            for y in range(2):
+                self.holes.append(pygame.math.Vector2((self.rect.width - (self.hole_offset * 2)) // 2 * x + self.hole_offset, (self.rect.height - (self.hole_offset * 2)) * y + self.hole_offset))
 
         if mode == "8-ball":
             r = ball_radius
@@ -93,9 +103,6 @@ class Table:
     def draw(self, draw_surface):
         self.surface.fill(self.color)
 
-        for ball in self.balls:
-            ball.draw(self.surface)
-
         # draw cue line
         if self.mouse_down_pos:
             pos = pygame.mouse.get_pos()  # gets SCREEN pos
@@ -105,7 +112,16 @@ class Table:
         # draw walls
         wall_rect = self.rect.copy()
         wall_rect.topleft = (0, 0)
-        pygame.draw.rect(self.surface, "black", wall_rect, self.wall_offset)
+        pygame.draw.rect(self.surface, "brown", wall_rect, self.wall_offset)
+
+        # draw holes
+        for hole_pos in self.holes:
+            pygame.draw.circle(self.surface, "black", hole_pos, self.hole_radius)
+
+        # draw balls
+        for ball in self.balls:
+            if ball.on_board:
+                ball.draw(self.surface)
 
         # draw game to screen
         draw_surface.blit(self.surface, self.rect)
@@ -150,6 +166,21 @@ class Table:
                     overlap = (min_dist - distance) / 2
                     current_ball.position += n * overlap
                     other_ball.position -= n * overlap
+
+            # --- 3. Hole Collisions ---
+            for hole_pos in self.holes:
+                delta_pos = current_ball.position - hole_pos
+                distance = delta_pos.length()
+                min_dist = current_ball.radius + self.hole_radius
+                if distance < min_dist:
+                    n = delta_pos.normalize()
+
+                    # Attract overlapping balls
+                    overlap = (min_dist - distance) / 2
+                    current_ball.position -= n * overlap * self.hole_strength
+
+                if distance < 15:
+                    current_ball.on_board = False
 
             # --- 2. Ball–Wall Collisions (within self.rect) ---
             # LEFT wall
